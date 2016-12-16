b54592ad7bce66d7ae01f92d4e5f8b71ea2a2ccf
Manuel Pichardo Marcano <manuelmarcano22@yahoo.com>
2016-12-16 17:46:29 -0600

    New Build

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
 docker build  -t dockerflask .
+if docker ps -a | grep --quiet flask3example; then 
 docker stop flask3example
-docker rm flask3example
-docker run --name flask3example -d -p 8000:8000 --net=host flask3
+docker rm flask3example ;
+fi
+docker run --name flask3example -d -p 8000:8000 --net=host dockerflask
 #Docker Hub
+
